604276fe1c85bf7bf1798e506a713e1c420c8630
Kamil Pustelnik <kpustelnik@student.agh.edu.pl>
2025-04-23 21:34:06 +0200

    Remove css, adjust configs

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,5 +1,9 @@
+"use client";
 import Image from "next/image";
-import styles from "./page.module.css";
+
+import * as React from 'react';
+import TextField from '@mui/material/TextField'
+
 
 export default function Home() {
   return (
