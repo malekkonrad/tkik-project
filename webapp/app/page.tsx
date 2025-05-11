1a379e3aec7cfbd360a414ded38dd552fa0bed34
Kamil Pustelnik <kpustelnik@student.agh.edu.pl>
2025-05-11 13:36:17 +0200

    add grid and fix labels

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -9,7 +9,7 @@ import * as React from 'react';
 import Select, { SelectChangeEvent } from '@mui/material/Select'
 import Checkbox from '@mui/material/Checkbox';
 import MenuItem from '@mui/material/MenuItem'
-import { Typography, Box, FormControlLabel, FormGroup } from '@mui/material';
+import { Typography, Box, FormControlLabel, FormGroup, FormControl, InputLabel, Grid } from '@mui/material';
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
@@ -111,153 +111,169 @@ export default function Home() {
   {/* ErrorListener https://tomassetti.me/antlr-mega-tutorial/#chapter19 */}
   return (
     <>
-      <Box>
-      <Select
-        label='Version'
-        value={version}
-        onChange={(event: SelectChangeEvent) => {
-          setVersion(event.target.value)
-        }}
-      >
-        {
-          Object.entries(compilers).map(([key, value]) => (<MenuItem key={key} value={key}>{value.name}</MenuItem>))
-        }
-      </Select><br />
-      <Select
-        label='Sample code'
-        value={sampleName}
-        onChange={(event: SelectChangeEvent) => setSampleName(event.target.value)}
-      >
-        <MenuItem value=''>No sample</MenuItem>
-        {
-          samples.map(x => (<MenuItem value={x.name} key={x.name}>{x.name}</MenuItem>))
-        }
-      </Select><br />
-      </Box>
-      <Box>
-        <Editor
-          height="90vh"
-          defaultLanguage="python"
-          defaultValue="// Put your code here"
-          onChange={value => setCode(value ?? '')}
-          onMount={(editor) => editorRef.current = editor}
-        />;
-      </Box>
-      <Box>
-        <FormGroup>
-          <FormControlLabel
-            label="Obfuscation"
-            control={
-              <Checkbox
-                checked={isObfuscated}
-                onChange={((e: React.ChangeEvent<HTMLInputElement>) => setObfuscated(e.target.checked))}
-              />
-            }
-          />
-          <FormControlLabel
-            label="Show Result"
-            control={
-              <Checkbox
-                checked={showResult}
-                onChange={((e: React.ChangeEvent<HTMLInputElement>) => setShowResult(e.target.checked))}
-              />
-            }
-          />
-          <FormControlLabel
-            label="Show Online Lua Compiler"
-            control={
-              <Checkbox
-                checked={showCompiler}
-                onChange={((e: React.ChangeEvent<HTMLInputElement>) => setShowCompiler(e.target.checked))}
-              />
-            }
-          />
-          <FormControlLabel
-            label="Show Diagram Tree"
-            control={
-              <Checkbox
-                checked={showDTree}
-                onChange={((e: React.ChangeEvent<HTMLInputElement>) => setShowDTree(e.target.checked))}
-              />
-            }
-          />
-          <FormControlLabel
-            label="Show Text Tree"
-            control={
-              <Checkbox
-                checked={showTTree}
-                onChange={((e: React.ChangeEvent<HTMLInputElement>) => setShowTTree(e.target.checked))}
-              />
-            }
-          />
-        </FormGroup>
-      </Box>
-      <Box>
-      {
-        (isObfuscated) ? (
-          <>
-            {
-              (showResult) ? (
-                <SyntaxHighlighter
-                  language="lua"
-                  style={dark}
-                  showLineNumbers={true}
-                >
-                  {result}
-                </SyntaxHighlighter>
-              ) : null
-            }
-            {
-              (showDTree) ? (<TldrawSvg parseTree={dTree} />) : null
-            }
-            {
-              (showTTree) ? (<Typography>{tTree}</Typography>) : null
-            }
-            {
-              (showCompiler) ? (
-                <>
-                  {/* Docs: https://onecompiler.com/apis/embed-editor */}
-                  <iframe
-                    frameBorder="0"
-                    height="450px"
-                    id='lua-result-editor'
-                    src={
-                      addURLParams('https://onecompiler.com/embed/lua', {
-                        hideLanguageSelection: 'true',
-                        hideNew: 'true',
-                        hideRun: 'false',
-                        hideNewFileOption: 'true',
-                        disableCopyPaste: 'false',
-                        disableAutoComplete: 'true',
-                        hideStdin: 'false',
-                        hideResult: 'false',
-                        hideTitle: 'true',
-                        theme: 'dark',
-                        listenToEvents: 'true',
-                        codeChangeEvent: 'true',
-                        hideEditorOptions: 'true'
-                      })
-                    }
-                    width="100%"
+      <Grid container spacing={2}>
+        <Grid size={6}>
+          <Box>
+            <FormControl>
+              <InputLabel id="version-label">Version</InputLabel>
+              <Select
+                labelId="version-label"
+                id="version-select"
+                label='Version'
+                value={version}
+                onChange={(event: SelectChangeEvent) => {
+                  setVersion(event.target.value)
+                }}
+              >
+                {
+                  Object.entries(compilers).map(([key, value]) => (<MenuItem key={key} value={key}>{value.name}</MenuItem>))
+                }
+              </Select><br />
+            </FormControl>
+            <FormControl>
+              <InputLabel id="samplecode-label">Sample code</InputLabel>
+              <Select
+                labelId="samplecode-label"
+                id="samplecode-select"
+                label='Sample code'
+                value={sampleName}
+                onChange={(event: SelectChangeEvent) => setSampleName(event.target.value)}
+              >
+                <MenuItem value=''>No sample</MenuItem>
+                {
+                  samples.map(x => (<MenuItem value={x.name} key={x.name}>{x.name}</MenuItem>))
+                }
+              </Select><br />
+            </FormControl>
+          </Box>
+          <Box>
+            <Editor
+              height="90vh"
+              defaultLanguage="python"
+              defaultValue="// Put your code here"
+              onChange={value => setCode(value ?? '')}
+              onMount={(editor) => editorRef.current = editor}
+            />;
+          </Box>
+        </Grid>
+        <Grid size={6}>
+          <Box>
+            <FormGroup>
+              <FormControlLabel
+                label="Obfuscation"
+                control={
+                  <Checkbox
+                    checked={isObfuscated}
+                    onChange={((e: React.ChangeEvent<HTMLInputElement>) => setObfuscated(e.target.checked))}
                   />
-                </>
-              ) : null
-            }
-          </>
-        ) : (
-          <Image
-            alt="Troll face"
-            src="https://www.google.com/imgres?q=troll%20face&imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2F7%2F73%2FTrollface.png&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FTrollface&docid=74T5DSmEo0ydQM&tbnid=XXYJjcpIM16s7M&vet=12ahUKEwi8o7fTpJuNAxVmCBAIHe60DBEQM3oECBgQAA..i&w=347&h=288&hcb=2&ved=2ahUKEwi8o7fTpJuNAxVmCBAIHe60DBEQM3oECBgQAA"
-            layout="responsive"
-          />
-        )
-      }
-      </Box>
+                }
+              />
+              <FormControlLabel
+                label="Show Result"
+                control={
+                  <Checkbox
+                    checked={showResult}
+                    onChange={((e: React.ChangeEvent<HTMLInputElement>) => setShowResult(e.target.checked))}
+                  />
+                }
+              />
+              <FormControlLabel
+                label="Show Online Lua Compiler"
+                control={
+                  <Checkbox
+                    checked={showCompiler}
+                    onChange={((e: React.ChangeEvent<HTMLInputElement>) => setShowCompiler(e.target.checked))}
+                  />
+                }
+              />
+              <FormControlLabel
+                label="Show Diagram Tree"
+                control={
+                  <Checkbox
+                    checked={showDTree}
+                    onChange={((e: React.ChangeEvent<HTMLInputElement>) => setShowDTree(e.target.checked))}
+                  />
+                }
+              />
+              <FormControlLabel
+                label="Show Text Tree"
+                control={
+                  <Checkbox
+                    checked={showTTree}
+                    onChange={((e: React.ChangeEvent<HTMLInputElement>) => setShowTTree(e.target.checked))}
+                  />
+                }
+              />
+            </FormGroup>
+          </Box>
+          <Box>
+          {
+            (isObfuscated) ? (
+              <>
+                {
+                  (showResult) ? (
+                    <SyntaxHighlighter
+                      language="lua"
+                      style={dark}
+                      showLineNumbers={true}
+                    >
+                      {result}
+                    </SyntaxHighlighter>
+                  ) : null
+                }
+                {
+                  (showDTree) ? (<TldrawSvg parseTree={dTree} />) : null
+                }
+                {
+                  (showTTree) ? (<Typography>{tTree}</Typography>) : null
+                }
+                {
+                  (showCompiler) ? (
+                    <>
+                      {/* Docs: https://onecompiler.com/apis/embed-editor */}
+                      <iframe
+                        frameBorder="0"
+                        height="450px"
+                        id='lua-result-editor'
+                        src={
+                          addURLParams('https://onecompiler.com/embed/lua', {
+                            hideLanguageSelection: 'true',
+                            hideNew: 'true',
+                            hideRun: 'false',
+                            hideNewFileOption: 'true',
+                            disableCopyPaste: 'false',
+                            disableAutoComplete: 'true',
+                            hideStdin: 'false',
+                            hideResult: 'false',
+                            hideTitle: 'true',
+                            theme: 'dark',
+                            listenToEvents: 'true',
+                            codeChangeEvent: 'true',
+                            hideEditorOptions: 'true'
+                          })
+                        }
+                        width="100%"
+                      />
+                    </>
+                  ) : null
+                }
+              </>
+            ) : (
+              <Image
+                alt="Troll face"
+                src="https://upload.wikimedia.org/wikipedia/en/7/73/Trollface.png"
+                layout="responsive"
+              />
+            )
+          }
+          </Box>
+        </Grid>
+      </Grid>
 
       <Box>
-        <Typography>Made with 💔 by:</Typography>
-        <Typography>- Kamil Pustelnik @ <a href='mailto:kpustelnik@student.agh.edu.pl'>kpustelnik@student.agh.edu.pl</a></Typography>
-        <Typography>- Konrad Małek @ <a href='mailto:kmalek@student.agh.edu.pl'>kmalek@student.agh.edu.pl</a></Typography>
+        <Typography align="center">Made with 💔 by:</Typography>
+        <Typography align="center">- Kamil Pustelnik @ <a href='mailto:kpustelnik@student.agh.edu.pl'>kpustelnik@student.agh.edu.pl</a></Typography>
+        <Typography align="center">- Konrad Małek @ <a href='mailto:kmalek@student.agh.edu.pl'>kmalek@student.agh.edu.pl</a></Typography>
       </Box>
     </>
   );
