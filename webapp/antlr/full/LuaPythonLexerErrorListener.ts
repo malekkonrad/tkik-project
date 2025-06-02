415748482f13cec0bca7ba9384b8b8f50ed34792
malekkonrad <konrad.malek1@gmail.com>
2025-06-02 17:38:28 +0200

    syntaxError

--- webapp/antlr/full/LuaPythonLexerErrorListener.ts
+++ webapp/antlr/full/LuaPythonLexerErrorListener.ts
@@ -1,7 +1,33 @@
 import { ErrorListener, RecognitionException, Recognizer } from 'antlr4'
+import PythonParser from './PythonParser';
+import PythonLexer from './PythonLexer';
+
 export default class LuaPythonLexerErrorListener extends ErrorListener<number> {
-  syntaxError(recognizer: Recognizer<number>, offendingSymbol: number, line: number, column: number, msg: string, err: RecognitionException | undefined): void {
-    console.log("Lexer", recognizer, offendingSymbol, line, column, msg, err) // TODO
-    throw new Error(`Lexer position ${line}:${column} `)
+  syntaxError(recognizer: Recognizer<number>, 
+    offendingSymbol: number, 
+    line: number, 
+    column: number, 
+    msg: string, 
+    err: RecognitionException | undefined): void {
+          console.log("Parser",recognizer, offendingSymbol, line, column, msg, err) 
+          let errorLine = "Unknown line";
+          var lexer = recognizer as PythonLexer; 
+    
+          var rule_name = lexer.ruleNames[offendingSymbol] || "Unknown Rule";
+          
+          if (lexer._input && lexer._input.toString) {
+            const sourceText = lexer._input.toString();
+            const lines = sourceText.split('\n');
+            if (line > 0 && line <= lines.length) {
+              errorLine = lines[line - 1]; 
+            }
+          }
+          var string_mess = 'Lexer Error at line ' + ':\n--' + errorLine + '\n--' +
+          ' '.repeat(column) + '^' + '\n' + "--" + msg;
+          if (rule_name != "Unknown Rule"){
+            string_mess += '\n--Rule: ' + rule_name;
+          }
+          
+          throw new Error(string_mess);
   }
 }
--- webapp/antlr/full/LuaPythonParserErrorListener.ts
+++ webapp/antlr/full/LuaPythonParserErrorListener.ts
@@ -1,7 +1,35 @@
-import { ErrorListener, Token, RecognitionException, Recognizer } from 'antlr4'
+import { ErrorListener, Token, RecognitionException, Recognizer} from 'antlr4'
+import PythonParser from './PythonParser';
+
 export default class LuaPythonParserErrorListener extends ErrorListener<Token> {
-  syntaxError(recognizer: Recognizer<Token>, offendingSymbol: Token, line: number, column: number, msg: string, err: RecognitionException | undefined): void {
-    console.log("Parser",recognizer, offendingSymbol, line, column, msg, err) // TODO
-    throw new Error(`Parser position ${line}:${column} `)
+
+  syntaxError(recognizer: Recognizer<Token>, 
+    offendingSymbol: Token, 
+    line: number, 
+    column: number, 
+    msg: string, 
+    err: RecognitionException | undefined): 
+    void {
+      console.log("Parser",recognizer, offendingSymbol, line, column, msg, err) 
+      let errorLine = "Unknown line";
+      recognizer as PythonParser; 
+
+      // ? - chyba nie potrzebne
+      var ctx = (recognizer as PythonParser).pattern_capture_target();
+      
+      if (offendingSymbol && offendingSymbol.getInputStream()) {
+      const inputStream = offendingSymbol.getInputStream();
+      if (inputStream && inputStream) {
+          const lines = inputStream.toString().split('\n');
+          if (line > 0 && line <= lines.length) {
+            errorLine = lines[line - 1]; // indexowanie od 1
+          }
+        }
+      }
+
+      var string_mess = 'Parser Error at line:\n--' + errorLine + '\n--' +
+      ' '.repeat(column) + '^' + '\n' + "--SyntaxError: " + msg;
+
+      throw new Error(string_mess);
+    }
   }
-}
